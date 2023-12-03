45f3ca8a5f6372c7b7adb5d6663ee82ab8e69f78
shreen-youssif <shreenyoussif22@gmail.com>
2023-12-03 06:40:36 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -7,7 +7,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @data: integer
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
@@ -15,13 +15,14 @@
  */
 typedef struct listint_s
 {
-	int data;
+	int n;
 	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t add_nodeint_end(listint_t *head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+
 int is_palindrome(listint_t **head);
-int aux_palindrome(listint_t **head, listint_t *end);
+int aux_palind(listint_t **head, listint_t *end);
 #endif /* LISTS_H */
