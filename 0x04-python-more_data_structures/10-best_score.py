c0a2f3f4969d1b7cc2f4c0dc2d0a2eba8728dabe
shreen-youssif <shreenyoussif22@gmail.com>
2023-12-06 10:25:14 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
     maximum_value = 0
-    key_1 = ""
-    for key , value in a_dictionary.items():
+    maximum_key= None
+    for key, value in a_dictionary.items():
         if value > maximum_value:
             maximum_value = value
-            key_1 = key
-    return key_1
+            maximum_key = key
+    return maximum_key
