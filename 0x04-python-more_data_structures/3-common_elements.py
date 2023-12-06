ddb493d712f00c2d23aa3d39abf6add7b03f41d8
shreen-youssif <shreenyoussif22@gmail.com>
2023-12-06 07:34:32 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    unique_set = set(my_list)
-    result = sum(unique_set)
-    return result
+def common_elements(set_1, set_2):
+    return set_1 & set_2
