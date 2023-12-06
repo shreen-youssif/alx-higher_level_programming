7a0b4deae5ce07fb336e29d04d7953fb91e8ed22
shreen-youssif <shreenyoussif22@gmail.com>
2023-12-06 11:48:21 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,8 +7,5 @@ def roman_to_int(roman_string):
     roman_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     for x in reversed(roman_string):
         number = roman_dict[x]
-        if total < number * 5:
-            total += number
-        else:
-            total -= number
+        total += number if total < number *5 else -num
     return total
