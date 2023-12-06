664bc0880d7d1d3387b0a80335225a1617149f12
shreen-youssif <shreenyoussif22@gmail.com>
2023-12-06 05:41:42 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    new_list = []
     for i in range(len(matrix)):
-        new_list = []
         for j in range(len(matrix[i])):
             new_list.append(matrix[i][j] * matrix[i][j])
     return new_list
