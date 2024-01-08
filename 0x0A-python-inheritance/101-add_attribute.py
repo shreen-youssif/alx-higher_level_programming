ac3bef0b16fecfd720ca6c95583ee5b06e99c3b8
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-08 09:29:46 -0800

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines Function to add aAttribute to an object if possible"""
+
+
+def add_attribute(obj, attr, value):
+    """
+    addition of not previous present attribute
+    :param obj: the Object to add attribute to
+    :param attr: what we call this attribute
+    :param value: the value of this attribute
+
+    :raises:
+    TypeError: if this attribute already present
+    """
+    if not hasattr(obj, attr):
+        setattr(obj, attr, value)
+    else:
+        raise TypeError("can't add new attribute")
