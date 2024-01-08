a79c0feb9d5f3e2dd8884301cc6152fdc5858555
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-08 08:48:15 -0800

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,10 +4,10 @@
 
 def is_kind_of_class(obj, a_class):
     """check if obj is subclass of a_class or not
-    
+
     :param obj: the object to be checked
     :param a_class: the class to compare to
-    
+
     :return:
         True: if obj is subclass of a_class
         otherwise false
