9159bebcbcebe2487f0df6a8bac38e09b0add2cb
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-09 12:52:39 -0800

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student module
+"""
+
+
+class Student:
+    """Defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiates a Student instance with first_name, last_name, and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance
+        """
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
