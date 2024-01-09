a61bdd846ec865eb03aac8e5d6bc5e2e7034f24f
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-09 09:49:38 -0800

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""""
+import sys
+
+if __name__ == "__main__":
+    save_file = __import__('5-save_to_json_file').save_to_json_file
+    load_file = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        objects = load_file("add_item.json")
+    except FileNotFoundError:
+        objects = []
+    objects.extend(sys.argv[1:])
+    save_file(objects, "add_item.json")
