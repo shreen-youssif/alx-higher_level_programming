70e3ec22561c76404b4af2ec7b23defaa1471438
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-15 07:25:41 -0800

    1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,3 +62,7 @@ class Rectangle(Base):
             raise ValueError("{} must be >= 0".format(name))
         elif not eq and value <= 0:
             raise ValueError("{} must be > 0".format(name))
+
+    def area(self):
+        '''Calculate the area of this rectangle'''
+        return self.width * self.height
