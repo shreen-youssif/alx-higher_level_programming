62cd8bda53ce45c9868378b997e49cbf264df3a1
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-15 08:09:49 -0800

    1

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,21 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def __modify(self, id=None, size=None, x=None, y=None):
+        '''modify square attributes by args'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''modify square attributes by keywords'''
+        if args:
+            self.__modify(*args)
+        elif kwargs:
+            self.__modify(**kwargs)
