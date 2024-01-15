a82a424ea4d0f94d6b999e16d446e0878c754ad4
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-15 12:01:33 -0800

    rectangle_tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Module for Rectangle unit tests.'''
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from random import randrange
+from contextlib import redirect_stdout
+import io
+
+
+class TestRectangle(unittest.TestCase):
+    '''Tests the Base class.'''
+
+    def setUp(self):
+        '''Imports module, instantiates class'''
+        Base._Base__nb_objects = 0
+
+    def tearDown(self):
+        '''Cleans up after each test_method.'''
+        pass
