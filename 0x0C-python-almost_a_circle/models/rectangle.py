a8dbaf907b488445dfdafde551dff3cbe50c80ec
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-15 07:31:42 -0800

    1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,9 @@ class Rectangle(Base):
         s = '\n' * self.y + \
             (' ' * self.x + '#' * self.width + '\n') * self.height
         print(s, end='')
+
+     def __str__(self):
+        '''Informal string represnetation of rectangle'''
+        return '[{}] ({}) {}/{} - {}/{}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width,
+                   self.height)
