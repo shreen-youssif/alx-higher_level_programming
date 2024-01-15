ca78bb9b60971b0f62bd99661e08946762445784
shreen-youssif <shreenyoussif22@gmail.com>
2024-01-15 08:37:46 -0800

    1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Basic class Base"""
+from json import dumps, loads
 
 
 class Base:
@@ -14,3 +15,11 @@ class Base:
         else:
             Base.__nb_object += 1
             self.id = Base.__nb_object
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''dumps list of dictionaries into json string'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
